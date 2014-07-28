e10c64ec3d99e85479ecd3a3af7a2fbc2060ca77
Henry Jackson <henry.jackson@dealacceleration.com>
2014-07-28 15:00:11 -0700

    comment out word

--- db/migrate/20140722191409_remove_column_time_created_from_ordln.rb
+++ db/migrate/20140722191409_remove_column_time_created_from_ordln.rb
@@ -6,6 +6,6 @@ class RemoveColumnTimeCreatedFromOrdln < ActiveRecord::Migration
     remove_column :ordlns, :expected_drive, :string
     remove_column :ordlns, :total_amount, :string
     remove_column :ordlns, :is_manually_closed, :string
-    remove_column :ordlns, :vendor_ref_list, :string
+    #remove_column :ordlns, :vendor_ref_list, :string
   end
 end
